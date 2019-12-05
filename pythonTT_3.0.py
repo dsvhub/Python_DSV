f4139b748abc4c5f2eff08cb42923e5ad55013ee
Norman Morgan <nmorgan826@gmail.com>
2019-12-05 15:20:00 -0500

    Inital commit

---
+++ pythonTT_3.0.py
@@ -0,0 +1,39 @@
+# Clear The Terminal or Screen
+import os
+os.system('clear')
+
+##############################################################
+# DATA TYPES
+# Strings
+# Numbers
+# Lists = (A List that can be edited)
+# Tuples = (a list that can not be edited)
+# Dictionaries = ( have a key and a value paired with that key)
+# Boolean = (True or False)
+##############################################################
+
+
+
+full_name = 'Norman Morgan'
+# print (full_name)
+age = 40  # numbers (Has no "")
+# names = ("John", "Mary", "Bob") # Lists and tupples 
+# print (names[2])
+
+# fav_pizza = {
+#	"Norman Morgan": "Extra Cheese",
+#	"Mary": "Sausage",
+#	"Bob": "Baccon"
+# }
+# print (fav_pizza["Bob"])
+
+name = True
+
+
+print (name)
+
+
+
+
+
+# /C/Users/nemes/Documents/My_Files/MyCodes/PytonProjects/PythonTT
